84c3c88ed4a65b6f9d84ef0be7b75171ab5f8b9f
oooh_shit <anastasiya.stecenko@gmail.com>
2025-02-25 00:22:23 +0300

    fix: fix loader

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Layout } from "./layout/Layout";
 export { Tabs } from "./tabs/Tabs";
 export { Table } from "./table/Table";
 export { Modal } from "./modal/Modal";
+export { Loader } from "./loader/Loader";

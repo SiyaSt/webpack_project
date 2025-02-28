09d9c229f741232aff21e35c6ff4855b16e26dff
oooh_shit <anastasiya.stecenko@gmail.com>
2025-02-28 16:38:27 +0300

    feat: add sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,6 @@ export { Input } from "./input/Input";
 export { Radio } from "./radio/Radio";
 export { Select } from "./select/Select";
 export { Switch } from "./switch/Switch";
+export { Sidebar } from "./sideBar/Sidebar";
+export { CustomLink } from "./customLink/CustomLink";
+export { Navigation } from "./navigation/Navigation";
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { useTheme } from "src/hooks/useTheme";
+import { classNames } from "src/shared/utils/ClassName";
+import { Sidebar } from "src/components";
+import "./Layout.scss";
 
 export const Layout = () => {
+  const { theme } = useTheme();
   return (
-    <>
-      <Outlet />
-    </>
+    <div className={classNames(`container ${theme}`)}>
+      <Sidebar />
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
   );
 };

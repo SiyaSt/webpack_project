826861e883e12f68052af1ec3caaa86cd7e3af4a
oooh_shit <anastasiya.stecenko@gmail.com>
2025-03-06 14:19:45 +0300

    fix: fix some minors

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@ export { Layout } from "./layout/Layout";
 export { Button } from "./button/Button";
 export { Checkbox } from "./checkbox/Checkbox";
 export { Input } from "./input/Input";
-export { Radio } from "./radio/Radio";
+export { RadioGroup } from "./radio/RadioGroup";
 export { Select } from "./select/Select";
 export { Switch } from "./switch/Switch";
--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,7 +1,8 @@
-export type Type = "primary" | "secondary" | "danger" | "link";
+export type Color = "primary" | "secondary" | "danger" | "link";
 export type Size = "small" | "medium" | "large";
 export type Variants = "outlined" | "filled" | "borderless" | "underlined";
 export interface Option {
+  id: string;
   value: string;
   label: string;
 }

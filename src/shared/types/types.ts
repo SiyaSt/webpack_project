49edadcf8e270f3ad2ab77d041b6c8fc63fc37bf
oooh_shit <anastasiya.stecenko@gmail.com>
2025-03-13 00:26:17 +0300

    fix: fix minors edits

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -2,7 +2,6 @@ export type Color = "primary" | "secondary" | "danger" | "link";
 export type Size = "small" | "medium" | "large";
 export type Variants = "outlined" | "filled" | "borderless" | "underlined";
 export interface Option {
-  id: string;
   value: string;
   label: string;
 }

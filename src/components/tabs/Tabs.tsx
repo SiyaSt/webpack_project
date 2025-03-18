444f339e6b42caa6b906d8fdaf0163ff2f11c5fe
oooh_shit <anastasiya.stecenko@gmail.com>
2025-03-18 23:52:35 +0300

    fix: tabs

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -4,13 +4,18 @@ import { classNames } from "src/shared/utils/ClassName";
 import "./Tabs.scss";
 
 export const Tabs: FC<TabsProps> = ({
-  tabs,
+  tabs = [],
   position = "top",
   size = "medium",
   color = "primary",
   orientation = "horizontal",
+  activeTab = 0,
 }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(activeTab);
+
+  if (tabs.length === 0) {
+    return null;
+  }
 
   return (
     <div
@@ -24,22 +29,24 @@ export const Tabs: FC<TabsProps> = ({
           `tabs--${orientation}`,
         )}
       >
-        {tabs.map((tab, index) => (
+        {tabs.map(({ label, icon, disabled }, index) => (
           <button
             key={index}
             className={classNames("tab", {
               active: activeIndex === index,
-              disabled: tab.disabled,
+              disabled: disabled,
             })}
-            onClick={() => !tab.disabled && setActiveIndex(index)}
-            disabled={tab.disabled}
+            onClick={() => !disabled && setActiveIndex(index)}
+            disabled={disabled}
           >
-            {tab.icon && <span className="tab-icon">{tab.icon}</span>}
-            {tab.label}
+            <div className="tab-container">
+              {icon && <span className="tab-icon">{icon}</span>}
+              <span className="tab-lable">{label}</span>
+            </div>
           </button>
         ))}
       </div>
-      <div className="tab-content">{tabs[activeIndex].content}</div>
+      <div className="tab-content">{tabs[activeIndex]?.content}</div>
     </div>
   );
 };
--- src/components/tabs/types.ts
+++ src/components/tabs/types.ts
@@ -1,9 +1,9 @@
-import { Orientation, Size, Type } from "src/shared/types/types";
+import { Orientation, Size, Color } from "src/shared/types/types";
 import { ReactNode } from "react";
 
 type TabPosition = "top" | "bottom" | "left" | "right";
 
-type TabItem = {
+export type TabItem = {
   label: string;
   content: ReactNode;
   icon?: ReactNode;
@@ -14,6 +14,7 @@ export interface TabsProps {
   tabs: TabItem[];
   position?: TabPosition;
   size?: Size;
-  color?: Type;
+  color?: Color;
   orientation?: Orientation;
+  activeTab?: number;
 }

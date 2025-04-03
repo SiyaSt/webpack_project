766f08b1a5c5b4bce12128ac10c7628cd3d9c86e
oooh_shit <anastasiya.stecenko@gmail.com>
2025-04-03 08:43:40 +0300

    fix: minors

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,5 +1,3 @@
-import { ComponentType } from "react";
-
 export type Color = "primary" | "secondary" | "danger" | "link";
 export type Size = "small" | "medium" | "large";
 export type Variants = "outlined" | "filled" | "borderless" | "underlined";

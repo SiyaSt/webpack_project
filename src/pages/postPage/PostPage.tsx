b6f641e3c49e6352c89f5f47f408379d337d81ae
oooh_shit <anastasiya.stecenko@gmail.com>
2025-04-29 17:02:25 +0300

    fix: fix scss files

--- src/pages/postPage/PostPage.tsx
+++ src/pages/postPage/PostPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { selectPosts } from "src/features/post/postSelector";
 import {

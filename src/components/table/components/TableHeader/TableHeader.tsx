12e6363c29a6f05095d40d37e7d7dc1fbdd5151f
oooh_shit <anastasiya.stecenko@gmail.com>
2025-05-08 03:00:30 +0300

    fix: add debounce to table

--- src/components/table/components/TableHeader/TableHeader.tsx
+++ src/components/table/components/TableHeader/TableHeader.tsx
@@ -1,5 +1,8 @@
 import { Input, Select } from "src/components";
 import { TableHeaderProps } from "./types";
+import { useState, useEffect } from "react";
+import { useDebounce } from "src/hooks/useDebounce";
+import { DEBOUNCE } from "src/shared/constants";
 
 export const TableHeader = <T extends object>({
   columns,
@@ -7,11 +10,26 @@ export const TableHeader = <T extends object>({
   onFilterChange,
   color,
 }: TableHeaderProps<T>) => {
+  const [localFilters, setLocalFilters] = useState<{ [key: string]: string }>(
+    filters,
+  );
+  const debouncedFilters = useDebounce(localFilters, DEBOUNCE);
+
+  useEffect(() => {
+    setLocalFilters(filters);
+  }, [filters]);
+
+  useEffect(() => {
+    Object.entries(debouncedFilters).forEach(([key, value]) => {
+      onFilterChange(key, value);
+    });
+  }, [debouncedFilters]);
+
   return (
     <thead>
       <tr>
         {columns.map((column) => (
-          <th key={column.accessorKey}>
+          <th key={column.accessorKey} className="column">
             {column.header}
             {column.filterType === "text" && (
               <Input
@@ -19,39 +37,43 @@ export const TableHeader = <T extends object>({
                 className="input-filter"
                 baseSize="small"
                 placeholder={`Filter ${column.header}`}
-                value={filters[column.accessorKey] || ""}
+                value={localFilters[column.accessorKey] || ""}
                 onChange={(e) =>
-                  onFilterChange(column.accessorKey, e.target.value)
+                  setLocalFilters((prev) => ({
+                    ...prev,
+                    [column.accessorKey]: e.target.value,
+                  }))
                 }
               />
             )}
-            {column.filterType === "select" && column.options && (
-              <Select
-                className="select-filter"
-                size="small"
-                variant="underlined"
-                color={color}
-                options={column.options.map((option) => ({
-                  value: option,
-                  label: option,
-                }))}
-                placeholder={`Filter ${column.header}`}
-                value={
-                  filters[column.accessorKey]
-                    ? {
-                        value: filters[column.accessorKey],
-                        label: filters[column.accessorKey],
-                      }
-                    : null
-                }
-                onChange={(selectedOption) =>
-                  onFilterChange(
-                    column.accessorKey,
-                    selectedOption?.value || "",
-                  )
-                }
-              />
-            )}
+            {column.filterType === "select" &&
+              Array.isArray(column.options) && (
+                <Select
+                  className="select-filter"
+                  size="small"
+                  variant="underlined"
+                  color={color}
+                  options={column.options.map((option) => ({
+                    value: option,
+                    label: option,
+                  }))}
+                  placeholder={`Filter ${column.header}`}
+                  value={
+                    filters[column.accessorKey]
+                      ? {
+                          value: filters[column.accessorKey],
+                          label: filters[column.accessorKey],
+                        }
+                      : null
+                  }
+                  onChange={(selectedOption) =>
+                    onFilterChange(
+                      column.accessorKey,
+                      selectedOption?.value || "",
+                    )
+                  }
+                />
+              )}
           </th>
         ))}
       </tr>
